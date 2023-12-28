ada56626dffa358577bc24e1513b7a4fd03f8d4a
Damjan Kovačič <letecnez@gmail.com>
2023-12-28 15:01:49 +0100

    2-38. App Finishing Touches

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -25,10 +25,10 @@ function GoalInput(props) {
 				/>
 				<View style={styles.buttonContainer}>
 					<View style={styles.button}>
-						<Button title="Add Goal" onPress={addGoalHandler} />
+						<Button title="Cancel" onPress={props.onCancel} color="#f31282" />
 					</View>
 					<View style={styles.button}>
-						<Button title="Cancel" onPress={props.onCancel} />
+						<Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
 					</View>
 				</View>
 			</View>
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		padding: 16,
-		backgroundColor: '#311b6b'
+		backgroundColor: '#311b6b',
 	},
 	image: {
 		width: 100,
@@ -53,9 +53,12 @@ const styles = StyleSheet.create({
 	},
 	textInput: {
 		borderWidth: 1,
-		borderColor: '#cccccc',
+		borderColor: '#e4d0ff',
+		backgroundColor: '#e4d0ff',
+		color: '#120438',
+		borderRadius: 6,
 		width: '100%',
-		padding: 8,
+		padding: 16,
 	},
 	buttonContainer: {
 		marginTop: 16,
