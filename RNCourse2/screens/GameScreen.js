dcdd3fac1ad444d6369f6d7ce51d6946faeece61
Damjan Kovacic <letecnez@gmail.com>
2024-01-02 12:44:00 +0100

    4-78. Finishing Touches

--- RNCourse2/screens/GameScreen.js
+++ RNCourse2/screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
     useEffect(() => {
         if (currentGuess === userNumber) {
-            onGameOver();
+            onGameOver(guessRounds.length);
         }
     }, [currentGuess, userNumber, onGameOver]);
 
@@ -79,7 +79,7 @@ function GameScreen({ userNumber, onGameOver }) {
                     </View>
                 </View>
             </Card>
-            <View>
+            <View style={styles.listContainer}>
                 <FlatList
                     data={guessRounds}
                     renderItem={(itemData) => (
@@ -107,5 +107,9 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         flex: 1,
+    },
+    listContainer: {
+        flex: 1,
+        padding: 16,
     },
 });
