93ff85e6e0d8c9e14c4e4577cc4801f41eaf7050
Damjan Kovacic <letecnez@gmail.com>
2024-01-07 23:25:59 +0100

    6-113. Bottom Tabs & App Finishing Touches

--- RNCourse3/App.js
+++ RNCourse3/App.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
@@ -18,10 +19,29 @@ function DrawerNavigator() {
                 headerStyle: { backgroundColor: '#351401' },
                 headerTintColor: 'white',
                 sceneContainerStyle: { backgroundColor: '#3f2f25' },
+                drawerContentStyle: {
+                    backgroundColor: '#351401',
+                },
+                drawerInactiveTintColor: 'white',
+                drawerActiveTintColor: '#351401',
+                drawerActiveBackgroundColor: '#e4baa1',
             }}
         >
-            <Drawer.Screen name="Categories" component={CategoriesScreen} options={{ title: 'All Categories' }} />
-            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+            <Drawer.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{
+                    title: 'All Categories',
+                    drawerIcon: ({ color, size }) => <Ionicons name="list" color={color} size={size} />,
+                }}
+            />
+            <Drawer.Screen
+                name="Favorites"
+                component={FavoritesScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => <Ionicons name="star" color={color} size={size} />,
+                }}
+            />
         </Drawer.Navigator>
     );
 }
