7aee3c0431335d1d1434d25d4bb186178dc569ab
Damjan Kovacic <letecnez@gmail.com>
2024-01-10 22:50:45 +0100

    8-133. Improving App Layout & Styling

--- RNCourse5/components/ExpensesOutput/ExpensesOutput.js
+++ RNCourse5/components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,5 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
@@ -12,7 +13,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
             <ExpensesList expenses={DUMMY_EXPENSES} />
         </View>
@@ -20,3 +21,11 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+        flex: 1,
+    },
+});
