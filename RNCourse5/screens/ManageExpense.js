d9507188fc405cdfbe3310aa3a53cc62b1c18a10
Damjan Kovacic <letecnez@gmail.com>
2024-01-21 16:11:48 +0100

    9-156. Setting & Using Default Values

--- RNCourse5/screens/ManageExpense.js
+++ RNCourse5/screens/ManageExpense.js
@@ -11,6 +11,8 @@ function ManageExpense({ route, navigation }) {
     const editedExpenseId = route.params?.expenseId;
     const isEditing = !!editedExpenseId;
 
+    const selectedExpense = expensesCtx.expenses.find((expense) => expense.id === editedExpenseId);
+
     useLayoutEffect(() => {
         navigation.setOptions({
             title: isEditing ? 'Edit Expense' : 'Add Expense',
@@ -41,6 +43,7 @@ function ManageExpense({ route, navigation }) {
                 submitButtonLabel={isEditing ? 'Update' : 'Add'}
                 onSubmit={confirmHandler}
                 onCancel={cancelHandler}
+                defaultValues={selectedExpense}
             />
 
             {isEditing && (
