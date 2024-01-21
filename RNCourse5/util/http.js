20543f07152f2d9a8614f313c95150c01d04c05f
Damjan Kovacic <letecnez@gmail.com>
2024-01-21 21:03:00 +0100

    10-167. Using Response Data from POST Requests

--- RNCourse5/util/http.js
+++ RNCourse5/util/http.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const BACKEND_URL = 'https://react-native-course-8a8ff-default-rtdb.europe-west1.firebasedatabase.app';
 
-export function storeExpense(expenseData) {
-    axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
+export async function storeExpense(expenseData) {
+    const response = await axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
+    const id = response.data.name;
+    return id;
 }
 
 export async function fetchExpenses() {
