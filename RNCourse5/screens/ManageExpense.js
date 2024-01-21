c167b85c6906a7705e93e3ce55d97ed751c9eed2
Damjan Kovacic <letecnez@gmail.com>
2024-01-21 21:10:51 +0100

    10-168. Updating & Deleting Backend Data (UPDATE & DELETE Requests)

--- RNCourse5/screens/ManageExpense.js
+++ RNCourse5/screens/ManageExpense.js
@@ -4,7 +4,7 @@ import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
-import { storeExpense } from '../util/http';
+import { deleteExpense, storeExpense, updateExpense } from '../util/http';
 
 function ManageExpense({ route, navigation }) {
     const expensesCtx = useContext(ExpensesContext);
@@ -20,7 +20,8 @@ function ManageExpense({ route, navigation }) {
         });
     }, [navigation, isEditing]);
 
-    function deleteExpenseHandler() {
+    async function deleteExpenseHandler() {
+        await deleteExpense(editedExpenseId);
         expensesCtx.deleteExpense(editedExpenseId);
         navigation.goBack();
     }
@@ -32,6 +33,7 @@ function ManageExpense({ route, navigation }) {
     async function confirmHandler(expenseData) {
         if (isEditing) {
             expensesCtx.updateExpense(editedExpenseId, expenseData);
+            await updateExpense(editedExpenseId, expenseData);
         } else {
             const id = await storeExpense(expenseData);
             expensesCtx.addExpense({ ...expenseData, id });
