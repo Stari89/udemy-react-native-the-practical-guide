c7b96d0e1cefa7d1abb60aadd450788177139318
Damjan Kovacic <letecnez@gmail.com>
2024-01-22 20:13:34 +0100

    11-182. Extracting the Authentication Token

--- RNCourse6/util/auth.js
+++ RNCourse6/util/auth.js
@@ -5,14 +5,14 @@ const API_KEY = 'AIzaSyDLqxsEzXX-tp_Z5OSK8FLsKqhno_7EKCA';
 async function authenticate(mode, email, password) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
     const response = await axios.post(url, { email, password, returnSecureToken: true });
-
-    console.log(response.data);
+    const token = response.data.idToken;
+    return token;
 }
 
-export async function createUser(email, password) {
-    await authenticate('signUp', email, password);
+export function createUser(email, password) {
+    return authenticate('signUp', email, password);
 }
 
-export async function login(email, password) {
-    await authenticate('signInWithPassword', email, password);
+export function login(email, password) {
+    return authenticate('signInWithPassword', email, password);
 }
