bbd25af59f68771fbc09b3d4a02d593e5e9e7f53
Damjan Kovacic <letecnez@gmail.com>
2024-01-23 22:56:52 +0100

    12-200. Creating a Custom Button

--- RNCourse7/components/Places/ImagePicker.js
+++ RNCourse7/components/Places/ImagePicker.js
@@ -1,7 +1,8 @@
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, View } from 'react-native';
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 import { useState } from 'react';
 import { Colors } from '../../constants/colors';
+import OutlinedButton from '../UI/OutlinedButton';
 
 function ImagePicker() {
     const [pickedImage, setPickedImage] = useState();
@@ -43,7 +44,9 @@ function ImagePicker() {
     return (
         <View>
             <View style={styles.imagePreview}>{imagePreview}</View>
-            <Button title="Take Image" onPress={takeImageHandler} />
+            <OutlinedButton icon="camera" onPress={takeImageHandler}>
+                Take Image
+            </OutlinedButton>
         </View>
     );
 }
