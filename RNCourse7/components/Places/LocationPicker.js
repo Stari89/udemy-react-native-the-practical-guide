738ef90b7c5e9f7e9aff5aff4ae272da4daefa29
Damjan Kovacic <letecnez@gmail.com>
2024-01-24 23:02:28 +0100

    12-207. Previewing Picked Locations

--- RNCourse7/components/Places/LocationPicker.js
+++ RNCourse7/components/Places/LocationPicker.js
@@ -2,14 +2,23 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import OutlinedButton from '../UI/OutlinedButton';
 import { Colors } from '../../constants/colors';
 import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getMapPreview } from '../../util/location';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute, useIsFocused } from '@react-navigation/native';
 
 function LocationPicker() {
     const [pickedLocation, setPickedLocation] = useState();
+    const isFocused = useIsFocused();
     const navigation = useNavigation();
+    const route = useRoute();
     const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+    useEffect(() => {
+        if (isFocused && route.params) {
+            const mapPickedLocation = { lat: route.params.pickedLat, lng: route.params.pickedLng };
+            setPickedLocation(mapPickedLocation);
+        }
+    }, [route, isFocused]);
 
     async function verifyPermissions() {
         if (
