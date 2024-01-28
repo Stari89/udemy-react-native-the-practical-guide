1c32b5ef8a8f1a4f650aec67af4d9f9708733a50
Damjan Kovacic <letecnez@gmail.com>
2024-01-28 15:20:56 +0100

    12-220. Showing a Readonly Map

--- RNCourse7/util/database.js
+++ RNCourse7/util/database.js
@@ -72,8 +72,13 @@ export function fetchPlaceDetails(id) {
     const promise = new Promise((resolve, reject) => {
         database.transaction((tx) => {
             tx.executeSql(`SELECT * FROM places WHERE id = ?`, [id], (_, result) => {
-                const p = result.rows._array[0];
-                const place = new Place(p.title, p.imageUri, { lat: p.lat, lng: p.lng, address: p.address }, p.id);
+                const dbPlace = result.rows._array[0];
+                const place = new Place(
+                    dbPlace.title,
+                    dbPlace.imageUri,
+                    { lat: dbPlace.lat, lng: dbPlace.lng, address: dbPlace.address },
+                    dbPlace.id,
+                );
                 resolve(place);
             }),
                 (_, error) => {
