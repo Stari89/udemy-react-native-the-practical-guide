7a46e679b8f435713db40c594908df58f19c2f33
Damjan Kovacic <letecnez@gmail.com>
2024-02-04 14:17:39 +0100

    15-254. Using the Push Token

--- RNCourse10/App.js
+++ RNCourse10/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, View } from 'react-native';
+import { Alert, Button, Platform, StyleSheet, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
 Notifications.setNotificationHandler({
@@ -15,19 +15,33 @@ Notifications.setNotificationHandler({
 
 export default function App() {
     useEffect(() => {
-        Notifications.getExpoPushTokenAsync().then((pushTokenData) => {
+        async function configurePushNotifications() {
+            const { status } = await Notifications.getPermissionsAsync();
+            let finalStatus = status;
+            if (finalStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+
+            if (finalStatus !== 'granted') {
+                Alert.alert('Permission required', 'Push notifications need the appropriate permissions.');
+                return;
+            }
+
+            const pushTokenData = await Notifications.getExpoPushTokenAsync();
             console.log(pushTokenData);
-        });
+
+            if (Platform.OS === 'android') {
+                Notifications.setNotificationChannelAsync('default', {
+                    name: 'default',
+                    importance: Notifications.AndroidImportance.DEFAULT,
+                });
+            }
+        }
+        configurePushNotifications();
     }, []);
 
     useEffect(() => {
-        (async () => {
-            const { status } = await Notifications.requestPermissionsAsync();
-            if (status !== 'granted') {
-                alert('Permission to show notifications has not been granted.');
-            }
-        })();
-
         const subscription1 = Notifications.addNotificationResponseReceivedListener((notification) => {
             console.log('NOTIFICATION RECEIVED');
             console.log(notification);
